11f52419051be3480ad57309d4d7556f4acb1c63
Calined <calined@gmx.de>
2021-09-09 10:15:09 +0200

    dragging and dropping unit object

---
+++ Assets/Unit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+    private Vector3 mouseOffset;
+
+    void OnMouseDown()
+    {
+        mouseOffset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + mouseOffset;
+        transform.position = curPosition;
+    }
+
+}
