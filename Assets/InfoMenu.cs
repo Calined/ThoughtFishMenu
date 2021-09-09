23d21302c17dfd307414bdaa51c0d8b910a7d8b5
Calined <calined@gmx.de>
2021-09-09 12:41:34 +0200

    now unit menu opens and closes

--- Assets/InfoMenu.cs
+++ Assets/InfoMenu.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class InfoMenu : MonoBehaviour
 {
 
+    void ShowMenu()
+    {
+        transform.GetComponent<SpriteRenderer>().enabled = true;
+        transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+        transform.GetChild(0).GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+
+    }
+
+    void HideMenu()
+    {
+        transform.GetComponent<SpriteRenderer>().enabled = false;
+        transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+        transform.GetChild(0).GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+
+    }
+
+
     public void ShowUnitInfo(string unitInfo)
     {
         transform.GetChild(0).GetChild(0).GetComponent<TextMesh>().text = unitInfo;
+
+        ShowMenu();
     }
 
 
