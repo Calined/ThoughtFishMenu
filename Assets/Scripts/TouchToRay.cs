b8b8abf622fa42aa591da30b2241e4a48c26c1e1
Calined <calined@gmx.de>
2021-09-09 11:59:10 +0200

    moved mouse tracking to raycast system

---
+++ Assets/Scripts/TouchToRay.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TouchToRay : MonoBehaviour
+{
+    public EventSystem myEventSystem;
+
+    Physics2DRaycaster m_Raycaster;
+    PointerEventData m_PointerEventData;
+
+    void Start()
+    {
+        m_Raycaster = GetComponent<Physics2DRaycaster>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        m_PointerEventData = new PointerEventData(myEventSystem);
+        m_PointerEventData.position = Input.mousePosition;
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        m_Raycaster.Raycast(m_PointerEventData, results);
+
+        foreach (RaycastResult result in results)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                result.gameObject.GetComponent<Unit>().LeftDown();
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                result.gameObject.GetComponent<Unit>().LeftUp();
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                result.gameObject.GetComponent<Unit>().RightDown();
+            }
+            if (Input.GetMouseButtonUp(1))
+            {
+                result.gameObject.GetComponent<Unit>().RightUp();
+            }
+
+        }
+    }
+
+
+}
+
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Unit : MonoBehaviour
 {
 
     private Vector3 mouseOffset;
 
-    void OnMouseDown()
+    public void LeftDown()
     {
         mouseOffset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+    }
+
+    public void LeftUp()
+    {
+
+    }
+
+    public void RightDown()
+    {
+
+    }
+
+    public void RightUp()
+    {
+
     }
 
     void OnMouseDrag()
