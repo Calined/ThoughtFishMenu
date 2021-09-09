725fd26b291f5878956a6c0572c7d4699c9613bd
Calined <calined@gmx.de>
2021-09-09 12:23:48 +0200

    implemented more fine click handling (only on proper up click)

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,24 +8,47 @@ public class Unit : MonoBehaviour
 
     private Vector3 mouseOffset;
 
+    private bool leftWasDown = false;
+    private bool rightWasDown = false;
+
     public void LeftDown()
     {
+        leftWasDown = true;
         mouseOffset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
     }
 
     public void LeftUp()
     {
+        if (leftWasDown)
+        {
+            LeftClick();
+            leftWasDown = false;
+        }
 
     }
 
     public void RightDown()
     {
-
+        rightWasDown = true;
     }
 
     public void RightUp()
     {
+        if (rightWasDown)
+        {
+            RightClick();
+            rightWasDown = false;
+        }
+    }
 
+    void LeftClick()
+    {
+        Debug.Log("LeftClick");
+    }
+
+    void RightClick()
+    {
+        Debug.Log("RightClick");
     }
 
     void OnMouseDrag()
@@ -33,6 +56,12 @@ public class Unit : MonoBehaviour
         Vector3 curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + mouseOffset;
         transform.position = curPosition;
+    }
+
+    void OnMouseExit()
+    {
+        leftWasDown = false;
+        rightWasDown = false;
     }
 
 }
